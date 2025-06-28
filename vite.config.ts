fdb70aaadd675b82a51a1d92fc87390a257b293e
jamaldevsecops <99463880+jamaldevsecops@users.noreply.github.com>
2025-06-28 13:59:35 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,10 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    port: 3000,
   },
   preview: {
-    port: 8080,
+    port: 3000,
     host: "::",
   },
   plugins: [
